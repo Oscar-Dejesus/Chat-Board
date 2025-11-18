7f1196f0b25f01edf8bbdc5b22467662b970e5b5
Oscar Dejesus <oscar6dejesus@gmail.com>
2025-11-18 12:02:44 -0500

    added cname and mobile UI

--- src/UI.js
+++ src/UI.js
@@ -7,7 +7,7 @@ function UI(){
         <div className="NavBar">
             
             <h1 className="Title">Welcome To The ChatBoard </h1>
-            <h1 style={{
+            <h1 className = "subtitle"style={{
     color: 'rgb(170, 207, 247)', 
     fontFamily: 'sans-serif'
   }}>Voice out anything you want here  </h1>
